fc8def6e6e1110b8223e69898516756d55cdbc99
Айдар Мингазов <aamingazov@gmail.com>
2019-03-23 21:07:14 +0300

    spec up

--- spec/features/user/account/update_spec.rb
+++ spec/features/user/account/update_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "Update Account" do
+feature "Update Account", js: true do
   include_context "current user signed in"
 
   background do
@@ -12,6 +12,13 @@ feature "Update Account" do
     click_on "Update"
 
     expect(page).to have_content("New Name")
+  end
+
+  scenario "User updates account avatar with valid data" do
+    attach_file "user[avatar]", Rails.root.join("spec", "support", "fixtures", "avatar.png")
+    click_on "Update"
+
+    expect(current_user.reload.avatar_identifier).to eq("avatar.png")
   end
 
   scenario "User enters not matched passwords" do
