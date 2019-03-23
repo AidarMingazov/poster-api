b818fac45506da468dfa232a81420170a8068a00
Айдар Мингазов <aamingazov@gmail.com>
2019-03-23 19:55:30 +0300

    up quality

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,12 +7,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url(*args)
+  def default_url
     "default-avatar.png"
   end
 
   def extension_whitelist
-    %w(jpg jpeg png)
+    %w[jpg jpeg png]
   end
 
   process resize_to_fit: [100, 100]
