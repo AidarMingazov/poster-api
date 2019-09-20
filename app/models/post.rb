dd3e629b4638f819422a06662cb5fca74cec6b70
Aidar Mingazov <aamingazov@gmail.com>
2019-09-20 19:12:51 +0300

    auto generate post url (#15)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   extend FriendlyId
 
-  friendly_id :name, use: :slugged
+  friendly_id :title, use: :slugged
 
   validates :body, :title, :user, presence: true
   validates :slug, uniqueness: { case_sensitive: false }, presence: true
