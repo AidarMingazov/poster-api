a454ed757e734741c1bb2fc7f1474e72a6f78fea
Aidar Mingazov <aamingazov@gmail.com>
2019-09-20 19:49:26 +0300

    [#168671639] Seed (#16)

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -1,1 +1,20 @@
-FactoryGirl.create(:user, email: "user@example.com")
+require 'factory_girl_rails'
+
+users_seeds = [
+  { email: "aamingazov@gmail.com" },
+  { email: "example1@example.com" },
+  { email: "example2@example.com" }
+]
+
+users_seeds.map do |user|
+  FactoryGirl.create :user, email: user[:email]
+end
+
+15.times do
+  FactoryGirl.create :post, user: User.all.sample
+end
+
+30.times do
+  FactoryGirl.create :comment, user: User.all.sample, post: Post.all.sample
+end
+
